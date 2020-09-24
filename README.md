bcb983f76c8ed5c39f4848efda4aefa353944851
Christina Flores <cdflori1@gmail.com>
2020-09-24 00:31:47 -0700

    added readme.md and fixed on script.js line

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # weatherapp
+
+ https://cdflori.github.io/weatherapp/
+
+For this assignment I created a weather dashboard and application using third party API OpenWeather app
+
+This application will allow the user to input a city in the search button and then see displayed the current weather of the city and a five day forecast below. The user will be able to search for multiple cities and have past searches stored and available to return to. 
+
+This application is currently not working. When adding in the last api search for the five day forecast the application stopped displaying the current city weather and past display. 
+
+Below are screen shots of what was working until last AJAX call was made.
+
+### User Will See Weather Dashboard
+
+![Weather Dashboard](assets/weatherdb.png)
+
+### User Will Enter City And See Displayed Weather
+
+![City Search](assets/citysearch.png)
+
--- script.js
+++ script.js
@@ -18,31 +18,27 @@ $("#button-addon2").on("click", function (event) {
     cityDisplay.addClass("card card-body");
     $("#cities").prepend(cityDisplay);
 
-    // Create an active link to display city data from past search buttons
-
-    // $("#cityDisplay").on("click", function(event){
-    //     let pastSearch = $(event.target).text();
-    //     localStorage.setItem("city", pastSearch)
-    //     cities.push(pastSearch);
-
-    //     currentWeather()
-    // })
-
 });
 
+// call for initial function of code
 renderWeather();
+
+// calling functions to display info lying in local storage
 
 function renderWeather() {
     currentWeather();
+    weeklyWeather();
 };
 
 function currentWeather() {
 
     let apiKey = "484a76af3afbc1a80eea350315a814e7";
+    // grabbing city input from local storage
     let city = localStorage.getItem("city");
 
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
+    // ajax call for current city weather
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -62,36 +58,34 @@ function currentWeather() {
             $("#humidity").text("Humidity | " + response.main.humidity + " % ");
             $("#wind").text("Humidity | " + response.wind.speed + " mph ");
 
+            // created variable to grab the longitude and latitude of city location
             let long = response.coord.lon;
             let lat = response.coord.lat;
 
             let uvIndexQuery = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + long;
 
+            // ajax call for UV index
             $.ajax({
 
                 url: uvIndexQuery,
                 method: "GET"
 
             })
-
+                // created an if else function to display UV levels and assigned color containers for uv values
                 .then(function (uv) {
 
                     if (uv.value < 3) {
                         $("#uv").html("UV Index: " + "<span id='low'>" + uv.value + "<span>");
                     }
-
                     if (uv.value < 5) {
                         $("#uv").html("UV Index: " + "<span id='moderate'>" + uv.value + "<span>");
                     }
-
                     if (uv.value < 7) {
                         $("#uv").html("UV Index: " + "<span id='high'>" + uv.value + "<span>");
                     }
-
                     if (uv.value < 10) {
                         $("#uv").html("UV Index: " + "<span id='very-high'>" + uv.value + "<span>");
                     }
-
                     else {
                         $("#uv").html("UV Index: " + "<span id='extreme'>" + uv.value + "<span>");
                     }
@@ -102,23 +96,39 @@ function currentWeather() {
 
         });
 
-        let forecastQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-        
-        $.ajax({
-            url: forecastQuery,
-            method: "GET"
-        })
 
-        .then(function(weekly){
+};
+
+// This portion was working above until I started the portion below. I am not exactly sure what went on?
+
+
+// Creating a function to display the current Weather
+function currentWeather() {
+
+    // Listing out variable for the apiKey to simplify query search
+    let apiKey = "484a76af3afbc1a80eea350315a814e7";
+    let city = localStorage.getItem("city");
+
+    let forecastQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    // ajax call to grab forecast data
+    $.ajax({
+        url: forecastQuery,
+        method: "GET"
+    })
+
+        .then(function (weekly) {
             console.log(weekly);
 
         })
 
-        let list = weekly.list;
-        console.log(list)
-        $("#forecast").empty()
+    let list = weekly.list;
+    console.log(list)
+    $("#forecast").empty()
 
-        for (let i = 0; i < list.length; i += 8) {
+    // Creating a for loop to grab every 8th element of days and hours then appending them to forecast div
+
+    for (let i = 0; i < list.length; i += 8) {
 
         let weeklyDate = list[i].dt_txt.split(" ")[0];
         let tempDiv = list[i].main.temp;
@@ -128,7 +138,7 @@ function currentWeather() {
         let divOne = $("<h5 class='card-title'>" + weeklyDate + '</h5>');
         $("#forecast").append(divOne)
 
-        let divTwo = $("<img alt='weather icon'" src='http://openweathermap.org/img/wn/' + imgDiv + '@2x.png/'> + '</img>/');
+        let divTwo = $("<img alt='weather icon' src='http://openweathermap.org/img/wn/'" + imgDiv + '@2x.png/' > + '</img>/');
         $("#forecast").append(divTwo)
 
         let divThree = $("<p class='card-text'> Temperature: " + tempDiv + "" + '&#176; F' + '</p>');
@@ -137,14 +147,10 @@ function currentWeather() {
         let divFour = $("<p class='card-text'> Humidity: " + humDiv + "" + '%' + '</p>');
         $("#forecast").append(divFour)
 
-        
+
         console.log(forecastQuery)
-    
 
-        }
-
-         
-
+    }
 };
 
 
